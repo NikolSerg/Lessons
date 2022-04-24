4516fcd564a271f5bb5aca77163d4450b8129b85
Sergey Nikolskiy <sseerrgg223819@gmail.com>
2022-04-24 16:48:10 +0300

    Methods changed

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,38 +5,63 @@ namespace Methods
 {
     internal class Program
     {
+        //static string[] StringToWords(string str)
+        //{
+        //    return str.Split(' ');
+        //}
+
+
+        /// <summary>
+        /// Возвращает массив подстрок данной строки отделенных разделителем
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        static string[] SplitStringToWords(string Str)
+        {
+            List<string> stroke = new List<string>();
+            string word = null;
+            foreach (char ch in Str)
+            {
+                if (ch != ' ') word += ch;
+                else
+                {
+                    stroke.Add(word);
+                    word = null;
+                }
+            }
+            stroke.Add(word);
+            return stroke.ToArray();
+        }
+        /// <summary>
+        /// Выводит в консоль элементы массива строк
+        /// </summary>
+        /// <param name="str"></param>
+        static void PrintStrings(string[] Str)
+        {
+            Console.WriteLine("Предложение разбитое на слова: ");
+            foreach (string word in Str)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        /// <summary>
+        /// Выводит в консоль строку обратную данной
+        /// </summary>
+        /// <param name="str"></param>
+        static void PrintReverseWords(string Str)
+        {
+            string[] words = SplitStringToWords(Str);
+            string reverseString = null;
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                reverseString += words[i] + " ";
+            }
+            Console.WriteLine($"Ваше предложение наоборот: {reverseString}");
+        }
+
         static void Main(string[] args)
         {
-            //static string[] StringToWords(string str)
-            //{
-            //    return str.Split(' ');
-            //}
-            
-            
-            static string[] SplitStringToWords(string str)
-            {
-                List<string> stroke = new List<string>();
-                string word = null;
-                foreach(char ch in str)
-                {
-                    if (ch != ' ') word += ch;
-                    else
-                    {
-                        stroke.Add(word);
-                        word = null;
-                    }
-                }
-                stroke.Add(word);
-                return stroke.ToArray();
-            }
-            static void PrintStrings(string[] str)
-            {
-                Console.WriteLine("Предложение разбитое на слова: ");
-                foreach (string word in str)
-                {
-                    Console.WriteLine(word);
-                }
-            }
 
             Console.Write("Введите предложение: ");
 
@@ -48,16 +73,6 @@ namespace Methods
 
 
             Console.Clear();
-            static void PrintReverseWords(string str)
-            {
-                string[] words = SplitStringToWords(str);
-                string reverseString = null;
-                for (int i = words.Length - 1; i >= 0; i--)
-                {
-                    reverseString += words[i] + " ";
-                }
-                Console.WriteLine($"Ваше предложение наоборот: {reverseString}");
-            }
 
             Console.Write("Введите предложение: ");
             PrintReverseWords(Console.ReadLine());
